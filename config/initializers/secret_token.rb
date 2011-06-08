cf4c47254bf99b2817caea6238bc014eac059ab8
Jeremy Gilman <jeremy.gilman@technekes.com>
2011-06-08 14:52:31 -0400

    Phase One

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+TestOne::Application.config.secret_token = 'bfb37e084464319cbd2f22f273f0be543798f6826a26ba9520f4a88fc6dd49f01480781709cd71e059958d2e6e655da0c0aae3dcd33186c3146549e9360beba0'
